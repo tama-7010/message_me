8bd15f315241517b9fdd665501321056254d7208
tama-7010 <otama.engine@gmail.com>
2019-08-07 23:38:30 +0900

    create Models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "John", password: "password")
+User.create(username: "Criss", password: "password")
+User.create(username: "Andrew", password: "password")
+User.create(username: "Black", password: "password")
+User.create(username: "Doh", password: "password")
